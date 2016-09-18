0396ebde93cbeebe03cf20488793459e4dc20711
johnmiller86 <chefjohn2006@hotmail.com>
2016-09-18 16:07:51 -0400

    Method Revision

--- app/src/main/java/com/ist_311/sliding_puzzle_miller_huynh_white/Activities/SettingsActivity.java
+++ app/src/main/java/com/ist_311/sliding_puzzle_miller_huynh_white/Activities/SettingsActivity.java
@@ -2,11 +2,15 @@ package com.ist_311.sliding_puzzle_miller_huynh_white.activities;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
@@ -97,17 +101,26 @@ public class SettingsActivity extends AppCompatActivity {
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_IMAGE) {
-                bitmap = null;
-                if (data != null) {
-                    try {
-                        bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+            if (requestCode == REQUEST_CAMERA){
+                //bitmap = (Bitmap) data.getExtras().get("data");
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
+                    Uri imageUri = data.getData();
+                    bitmap = fixOrientation(bitmap, imageUri);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                imageView.setImageBitmap(bitmap);
-
+            }
+            else if (requestCode == SELECT_IMAGE) {
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
+                    Uri imageUri = data.getData();
+                    bitmap = fixOrientation(bitmap, imageUri);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 //                // Store in SQL
 //                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 //                bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
@@ -120,5 +133,31 @@ public class SettingsActivity extends AppCompatActivity {
 //                imageView.setImageBitmap(bitmap);
             }
         }
+    }
+
+    /**
+     * Rotates bitmap to correct orientation.
+     * @param bitmap the image bitmap.
+     * @param imageUri the image uri.
+     * @return the properly oriented bitmap.
+     */
+    private Bitmap fixOrientation(Bitmap bitmap, Uri imageUri){
+
+        try {
+            String[] orientationColumn = {MediaStore.Images.Media.ORIENTATION};
+            Cursor cursor = getContentResolver().query(imageUri, orientationColumn, null, null, null);
+            int orientation = -1;
+            if (cursor != null && cursor.moveToFirst()) {
+                orientation = cursor.getInt(cursor.getColumnIndex(orientationColumn[0]));
+            }
+            cursor.close();
+            Matrix matrix = new Matrix();
+            matrix.postRotate(orientation);
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        }
+        catch (Exception e) {
+            Log.d(SettingsActivity.class.getSimpleName(), e.getMessage());
+        }
+        return bitmap;
     }
 }
